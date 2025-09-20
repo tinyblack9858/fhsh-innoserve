5d2d12d121270387d8ad15b73d8bcb41862a7213
zyyuyou_ <yoyocheng0811@gmail.com>
2025-09-21 02:06:55 +0800

    Update preload.js
    修改程式碼

--- src/preload.js
+++ src/preload.js
@@ -3,9 +3,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 // 建立一個安全的橋樑，將主進程的功能暴露給前端 UI
 contextBridge.exposeInMainWorld('electronAPI', {
   // 暴露「開始監控」功能
-startMonitoring: (config) => ipcRenderer.send('start-monitoring', config),
+  startMonitoring: (config) => ipcRenderer.send('start-monitoring', config),
   // 暴露「停止監控」功能
-stopMonitoring: () => ipcRenderer.send('stop-monitoring'),
+  stopMonitoring: () => ipcRenderer.send('stop-monitoring'),
   // 暴露一個監聽器，讓前端可以接收來自後端的事件
-onPythonEvent: (callback) => ipcRenderer.on('python-event', (event, data) => callback(data))
+  onPythonEvent: (callback) => ipcRenderer.on('python-event', (event, data) => callback(data))
 });
